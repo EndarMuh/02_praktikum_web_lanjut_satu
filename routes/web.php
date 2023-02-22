7f810bfab62f65136beef4ddb20ac70e6a05393e
endarmuh <2141720160@student.polinema.ac.id>
2023-02-22 09:24:09 +0700

    Perubahan

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Muhammad Endar Darmawan | 2141720160";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID ".$id;
 });
